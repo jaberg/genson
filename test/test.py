99fb6e0e835fd3b980af6037858aa99014c07115
James Bergstra <james.bergstra@gmail.com>
2011-10-20 13:53:40 -0400

    tricky unit test

--- test/test.py
+++ test/test.py
@@ -1,8 +1,25 @@
+import unittest
 import genson
 import time
 
-if __name__ == "__main__":
-    
+def test_simple():
+    testdata2 = """
+        {
+           "test0": 4,
+           "test1" : <0,1,2>,
+           "test3" : <"a", "b", uniform(0,1)>,
+           ("test4", "test5") : (0, 1),
+           ("test6", "test7") : 1,
+           ("test8","test9") : <("d", "e"), ("f", "g")>
+        }
+    """
+    tic = time.time()
+    son_iterator1 = genson.loads(testdata2)
+    print list(son_iterator1)
+    toc = time.time() - tic
+    print("Simple example parse time: %s" % toc)
+
+def test_complex():
     testdata = """
        {
            "test0": 4,
@@ -25,31 +42,15 @@ if __name__ == "__main__":
            "testZ": 10
        }
        """
-    testdata2 = """
-        {
-           "test0": 4,
-           "test1" : <0,1,2>,
-           "test3" : <"a", "b", uniform(0,1)>,
-           ("test4", "test5") : (0, 1), 
-           ("test6", "test7") : 1,
-           ("test8","test9") : <("d", "e"), ("f", "g")>
-        }
-    """
-      
-    tic = time.time()
-    son_iterator1 = genson.loads(testdata2)
-    toc = time.time() - tic
-    print("Simple example parse time: %s" % toc)
-           
     tic = time.time()
     son_iterator2 = genson.loads(testdata)
     toc = time.time() - tic
     print("Complex example parse time: %s" % toc)
-    
-    
     for d in son_iterator2:
         print d
     print
+
+def test_from_file():
 
     print "Again, but from a formatted file:"
     with open('test.gson') as f:
@@ -57,3 +58,14 @@ if __name__ == "__main__":
 
     for d in son_file_iterator:
         print d
+
+class Tricky(unittest.TestCase):
+
+    def test_trailing_comma(self):
+        # currently this fails, which is a shame
+        testdata2 = """{ "test0": 4, } """
+        list(genson.loads(testdata2))
+
+    def test_dup_keys(self):
+        # currently this does not fail, which is a shame
+        self.assertRaises(Exception, genson.loads, '{ "test0": 4, "test0": 5 }')
